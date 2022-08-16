2b7de6e02294b7d8c251eb5b8fe63c94a9280b25
atenahadavi <atenahadavi@gmail.com>
2022-08-16 20:25:08 +0430

    install tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,17 @@
                 </div>
             @endif
 
+            <div x-data="{count: 0 }">
+
+<span x-text="count"></span>
+
+<button x-on:click="count++"> click me</button>
+            </div>
+
+
+
+
+
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
@@ -128,5 +139,6 @@
                 </div>
             </div>
         </div>
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </body>
 </html>
